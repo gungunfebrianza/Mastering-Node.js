e69d68c391aa21925d09b6a2d9e5423b996b759d
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-05-07 01:31:48 +0700

    Rename Files

--- A1. Basic/app.js
+++
@@ -1,6 +0,0 @@
-console.log('Hello world!');
-const fs = require('fs');
-fs.appendFile('message.txt', 'data to append', (err) => {
-  if (err) throw err;
-  console.log('The "data to append" was appended to file!');
-});

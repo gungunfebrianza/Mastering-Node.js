2310658ee94bdc8f4bc9a62acf19f6b54549a0e0
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-05-08 01:57:49 +0700

    Add Readline API

--- A1. Basic/5.Readline.js
+++ A1. Basic/5.Readline.js
@@ -5,10 +5,10 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.on('line', (input) => {
-  console.log(`Received ::: ${input}`);
-});
-
+/*
+The readline module provides an interface for reading data from a Readable stream (
+such as process.stdin) one line at a time.
+*/
 rl.question('What do you think of Node.js? ', (answer) => {
   console.log(`Thank you for your valuable feedback: ${answer}`);
   rl.on('line', (input) => {

a935fe227462dae8ff0d1b861e315a96f6fdf8e3
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-05-07 07:27:47 +0700

    Update Filesystem Example - appendFile

--- A1. Basic/2.fs.appendFile.js
+++ A1. Basic/2.fs.appendFile.js
@@ -1,5 +1,22 @@
 const fs = require('fs');
-fs.appendFile('message.txt', 'data to append', (err) => {
+
+/*
+Asynchronously append data to a file, creating the file if it does not yet exist.
+Data can be a string or a Buffer.
+
+fs.appendFile(file, data[, options], callback)*/
+fs.appendFile('message.txt', 'data to append', 'utf8', (err) => {
   if (err) throw err;
   console.log('The "data to append" was appended to file!');
 });
+
+/*
+file <string> | <Buffer> | <URL> | <number> filename or file descriptor
+data <string> | <Buffer>
+options <Object> | <string>
+    encoding <string> | <null> Default: 'utf8'
+    mode <integer> Default: 0o666
+    flag <string> See support of file system flags. Default: 'a'.
+callback <Function>
+    err <Error>
+*/

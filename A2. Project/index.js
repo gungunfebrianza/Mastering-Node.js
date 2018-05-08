aa4f96427418f7ef81f7513717a728fb712f1fc5
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-05-08 12:17:30 +0700

    Add uniq method example

--- A2. Project/index.js
+++ A2. Project/index.js
@@ -13,3 +13,6 @@ console.log(_.isString('Gary'));
 // var user = os.userInfo();
 //
 // fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
+
+var filteredArray = _.uniq(['Gary', 1, 'Gary', 1, 2, 3, 4]);
+console.log(filteredArray);

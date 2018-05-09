50f0bee62de1b3806af38ae450235044bcd3e2e2
daddybitcoin <pusatrisetteknologi@gmail.com>
2018-05-10 06:12:49 +0700

    Add Example App is Number?

---
+++ Demos/appisnumber.js
@@ -0,0 +1,23 @@
+const isNumber = require('is-number');
+
+isNumber(5e3);               // true
+isNumber(0xff);              // true
+isNumber(-1.1);              // true
+isNumber(0);                 // true
+isNumber(1);                 // true
+isNumber(1.1);               // true
+isNumber(10);                // true
+isNumber(10.10);             // true
+isNumber(100);               // true
+isNumber('-1.1');            // true
+isNumber('0');               // true
+isNumber('012');             // true
+isNumber('0xff');            // true
+isNumber('1');               // true
+isNumber('1.1');             // true
+isNumber('10');              // true
+isNumber('10.10');           // true
+isNumber('100');             // true
+isNumber('5e3');             // true
+isNumber(parseInt('012'));   // true
+console.log(isNumber(parseFloat('012'))); // true

72b772deaa4f18a5e4129bf2ec3f9753ec1a224a
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-06 20:45:07 +0700

    File System Open

--- API - File System/exists.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-fs.exists('example.file', (exists) => {
-  console.log(exists ? '\nit\'s there' : 'no passwd!');
-});
---
+++ API - File System/open.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+/*
+    path <string> | <Buffer> | <URL>
+    Returns: <boolean>
+    Returns true if the path exists, false otherwise.
+*/
+
+fs.open('example.file', 'wx', (err, fd) => {
+  if (err) {
+    if (err.code === 'EEXIST') {
+      console.error('example.file already exists');
+      return;
+    }
+    throw err;
+  }
+  //writeMyData(fd);
+});
+
+fs.open('myfile', 'r', (err, fd) => {
+  if (err) {
+    if (err.code === 'ENOENT') {
+      console.error('myfile does not exist');
+      return;
+    }
+
+    throw err;
+  }
+  //readMyData(fd);
+});

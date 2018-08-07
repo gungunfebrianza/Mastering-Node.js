7056da7d37e3574ce3517ba10d3a9dc1c16fd90f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-08 00:23:48 +0700

    Fix Names & Module Example

---
+++ 2. Module/1.module.js
@@ -0,0 +1,7 @@
+module.exports.title = 'I am title \n';
+module.exports.calculation = ()=>{
+  console.log('calculation');
+}
+
+console.log(module.exports.title);
+console.log(module.exports.calculation());

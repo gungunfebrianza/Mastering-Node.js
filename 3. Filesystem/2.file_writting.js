a4f7092fc23b2cc13124ac8d128029a7cbd16878
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-08 00:54:13 +0700

    Append File Writting Example

--- 3. Filesystem/2.file_writting.js
+++ 3. Filesystem/2.file_writting.js
@@ -4,3 +4,11 @@ fs.writeFile('data.html', 'Hello World', 'UTF-8', (err)=>{
   if(err) return err;
   console.log('File Saved!');
 })
+
+fs.appendFile('data.html', ' data to append', 'utf8', (err) => {
+  if (err) throw err;
+/*  if (err) {
+    console.log('Unable to write to file');
+  }*/
+  console.log('The "data to append" was appended to file!');
+});

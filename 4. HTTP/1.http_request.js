742d1216210bae2a0523210c32a4615616eacb2a
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-08 10:00:04 +0700

    Example HTTPS Request

---
+++ 4. HTTP/1.http_request.js
@@ -0,0 +1,23 @@
+const https = require('https');
+const fs = require('fs');
+
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+https.get(url, res=>{
+  res.setEncoding('UTF-8');
+  let body = '';
+
+  res.on('data', data=>{
+    body += data;
+  });
+
+  res.on('end', ()=>{
+    // body = JSON.parse(body);
+    // console.log(`${body[0].title}`);
+
+    // fs.writeFile('data.json', body, 'UTF-8', (err)=>{
+    //   if (err) return err;
+    //   console.log('Data Success Pulled!');
+    // });
+  });
+})

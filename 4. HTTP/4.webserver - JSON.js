9ef5ce98bd6e7b53c2a3f1e1df9418f76cf8cd49
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-08 16:42:53 +0700

    Fix Naming Convention

--- 4. HTTP/4.webserver - JSON.js
+++ 4. HTTP/4.webserver - JSON.js
@@ -5,19 +5,23 @@ const fs = require('fs');
 const url = 'https://jsonplaceholder.typicode.com/posts';
 
 http.createServer((request, server_response)=>{
+
   if (request.method === 'GET' && request.url === '/posts') {
     https.get(url, (http_response)=>{
       http_response.on('data', data=>{
         http_response.setEncoding('utf8');
-        console.log(data);
+        server_response.write(data);
       });
 
       http_response.on('end', ()=>{
         server_response.end();
         console.log('its over');
-      })
+      });
 
-    })
+    });
+  } else {
+    server_response.writeHead(404,{'Content-Type':'text/plain'});
+    server_response.end('404 ERROR');
   }
 }).listen(666);
 console.log('Server is listening!');

ab65996144f7d020e8cf199bd710a5af2c14474b
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-10 03:16:46 +0700

    Add Comment

--- 5. ServerDev/3.express_route.js
+++ 5. ServerDev/3.express_route.js
@@ -3,6 +3,10 @@ let app = express();
 
 const port = process.env.PORT || 9999;
 
+// // route method
+// // ExpressJS route methods have equivalent names to HTTP verbs.
+// For instance: app.get() for the GET HTTP verb or
+// app.delete() for the DELETE HTTP verb.
 app.get('/', (req, res)=>{
   res.send('<h1>Hello World!</h1>');
 });
--- 5. ServerDev/4.express_routehandler.js
+++ 5. ServerDev/4.express_routehandler.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+// Route handlers are callback functions that accept three arguments. 
+// The first one is the request object, the second one is the response object,
+// and the last one is a callback, which passes the handler to the next request
+// handler in the chain.
 app.get('/one', (request, response, nextHandler) => {
 	response.type('text/plain')
 	response.write('Hello ')

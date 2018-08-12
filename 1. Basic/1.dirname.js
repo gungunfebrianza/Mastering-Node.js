2631c29c773a55e1aec6a2b721eb9326d341f9e3
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-13 05:05:23 +0700

    Path API

--- 1. Basic/1.dirname.js
+++ 1. Basic/1.dirname.js
@@ -1,1 +1,11 @@
+const path = require('path');
+
+//this is global object from Node.js API
+//The directory name of the current module.
 console.log(__dirname);
+
+//this is equivalent to :
+console.log(path.dirname(__filename));
+
+//Output :
+//C:\xampp\htdocs\front-end\Mastering-Node.js\1. Basic

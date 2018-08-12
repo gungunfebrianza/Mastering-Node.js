e5be158835ac9a6d93beca84544cbe09383e08fa
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-13 05:08:53 +0700

    Windows Consistency

--- 1. Basic/2.filename.js
+++ 1. Basic/2.filename.js
@@ -1,11 +1,17 @@
 const path = require('path');
 
-//this is global object from Node.js API
-//The file name of the current module. This is the resolved absolute path of the current module file.
+/*this is global object from Node.js API
+The file name of the current module. This is the resolved absolute path of
+the current module file.*/
 console.log(__filename);
-//Output :
-//C:\xampp\htdocs\front-end\Mastering-Node.js\1. Basic\2.filename.js
+/*Output :
+C:\xampp\htdocs\front-end\Mastering-Node.js\1. Basic\2.filename.js
+*/
 
-console.log(path.basename(__filename));
-//Output :
-//2.filename.js
+
+/*To achieve consistent results when working with Windows file paths on any
+operating system, use path.win32 */
+console.log(path.win32.basename(__filename));
+/*Output :
+2.filename.js
+*/

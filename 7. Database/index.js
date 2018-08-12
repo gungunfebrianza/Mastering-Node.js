306a05a68870e5f3ba0d6c962adcf8262cda8680
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-13 02:36:38 +0700

    Restful App Success

--- 7. Database/index.js
+++ 7. Database/index.js
@@ -1,13 +1,22 @@
-var MongoClient = require('mongodb').MongoClient
+const mongoose = require('mongoose');
 
-MongoClient.connect('mongodb://localhost:27017/animals', function (err, client) {
-  if (err) throw err
-
-  var db = client.db('animals')
-
-  db.collection('mammals').find().toArray(function (err, result) {
-    if (err) throw err
-
-    console.log(result)
-  })
+mongoose.connect('mongodb://localhost:27017/animals');
+mongoose.connection.once('open', ()=> console.log('connected!')).on('error', (err)=>{
+  console.log('could not connect!', err);
 })
+
+
+
+// var MongoClient = require('mongodb').MongoClient
+//
+// MongoClient.connect('mongodb://localhost:27017/animals', function (err, client) {
+//   if (err) throw err
+//   console.log('connected');
+//   // var db = client.db('animals')
+//   //
+//   // db.collection('mammals').find().toArray(function (err, result) {
+//   //   if (err) throw err
+//   //
+//   //   console.log(result)
+//   // })
+// })

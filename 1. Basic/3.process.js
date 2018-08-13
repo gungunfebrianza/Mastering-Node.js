a25227da4455bb1331a7b7107044eb2ac07ac38f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-13 14:44:02 +0700

    update basic example

--- 1. Basic/3.process.js
+++ 1. Basic/3.process.js
@@ -1,5 +1,21 @@
 console.log(process.argv);
 
+/*The process object is a global that provides information about, and control
+over, the current Node.js process. As a global, it is always available to
+Node.js applications without using require().*/
+
 let flag = process.argv.indexOf('--user');
 
 console.log(flag);
+
+/*Try this input
+node process test e --user
+*/
+
+/*Output
+0: C:\Program Files\nodejs\node.exe
+1: C:\xampp\htdocs\front-end\Mastering-Node.js\API - Process\process.argv
+2: test
+3: e
+4: --user
+*/

9a2d97f5384732876c3be37d05cd6e573309864c
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-15 04:58:00 +0700

    get request time

---
+++ 5. ServerDev/middleware_reqtime.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+
+var requestTime = function (req, res, next) {
+  req.requestTime = Date.now()
+  next()
+}
+
+app.use(requestTime)
+
+app.get('/', function (req, res) {
+  var responseText = 'Hello World!<br>'
+  responseText += '<small>Requested at: ' + req.requestTime + '</small>'
+  res.send(responseText)
+})
+
+app.listen(9999)

b3707c0bbdfa96c4926f149723e1f270e6c1d64d
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-14 21:15:16 +0700

    process ppid

---
+++ API - Process/process.ppid.js
@@ -0,0 +1,5 @@
+//The process.pid property returns the PID of the process.
+console.log(`This process is pid ${process.pid}`);
+
+//The process.ppid property returns the PID of the current parent process.
+console.log(`The parent process is pid ${process.ppid}`);

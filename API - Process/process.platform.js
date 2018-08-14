399bfa77b8a37ddd7b5a799e4cb0cc934ca9c027
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-14 21:06:41 +0700

    Process Platform

---
+++ API - Process/process.platform.js
@@ -0,0 +1,2 @@
+//The process.platform property returns a string identifying the operating system platform on which the Node.js process is running.
+console.log(`This platform is ${process.platform}`);

801c52fd167ca9c1242190b4ebfac4cdd5f29c57
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-16 01:56:07 +0700

    Add Example : modular app

---
+++ Express.js/1.routing/8.module_bird.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var router = express.Router()
+
+// middleware that is specific to this router
+router.use(function timeLog (req, res, next) {
+  console.log('Time: ', Date.now())
+  next()
+})
+// define the home page route
+router.get('/', function (req, res) {
+  res.send('Birds home page')
+})
+// define the about route
+router.get('/about', function (req, res) {
+  res.send('About birds')
+})
+
+module.exports = router
---
+++ Express.js/1.routing/9.test.js
@@ -0,0 +1,11 @@
+const express = require('express')
+var birds = require('./8.module_bird')
+
+const app = express()
+
+app.use('/birds', birds)
+
+app.listen(
+	9999,
+	() => console.log('Web Server running on port 9999'),
+)

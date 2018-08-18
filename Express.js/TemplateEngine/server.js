8459d565205e5acaeb0fdf109c51a3525780a5a2
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-18 12:54:25 +0700

    Partials

--- Express.js/TemplateEngine/server.js
+++ Express.js/TemplateEngine/server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const hbs = require('hbs');
 
 var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerHelper('getCurrentYear', () => {
+  //return 'test';
+  return new Date().getFullYear()
+});
+
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
@@ -13,14 +20,11 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'Andrew',
-    likes: [
-      'Biking',
-      'Cities'
-    ]
-  });
+  res.render('home.hbs', {
+     pageTitle: 'Home Page',
+     welcomeMessage: 'Welcome to my website',
+     currentYear: new Date().getFullYear()
+   });
 });
 
 app.listen(9999);

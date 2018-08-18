7c7c83d035b9992a9a3dac8518d6cd7194c17ee7
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-18 13:13:07 +0700

    Rearrange Document

--- 5. Express.js/2. Middleware/1.middleware.js
+++ 5. Express.js/2. Middleware/1.middleware.js
@@ -1,6 +1,3 @@
-/*To load the middleware function, call app.use(), specifying the middleware
-function. For example, the following code loads the myLogger middleware
-function before the route to the root path (/).*/
 var express = require('express')
 var app = express()
 
@@ -9,6 +6,9 @@ var myLogger = function (req, res, next) {
   next()
 }
 
+/*To load the middleware function, call app.use(), specifying the middleware
+function. For example, the following code loads the myLogger middleware
+function before the route to the root path (/).*/
 app.use(myLogger)
 
 app.get('/', function (req, res) {
--- 5. Express.js/3. TemplateEngine/server.js
+++ 5. Express.js/3. TemplateEngine/server.js
@@ -5,8 +5,10 @@ var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 hbs.registerHelper('getCurrentYear', () => {
-  //return 'test';
   return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
 });
 
 app.set('view engine', 'hbs');
@@ -15,7 +17,6 @@ app.use(express.static(__dirname + '/public'));
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
   });
 });
 
@@ -23,7 +24,6 @@ app.get('/', (req, res) => {
   res.render('home.hbs', {
      pageTitle: 'Home Page',
      welcomeMessage: 'Welcome to my website',
-     currentYear: new Date().getFullYear()
    });
 });
 

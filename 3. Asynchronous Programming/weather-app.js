cbb6cdaeee34e8ff287aab53502a84b6a5221aff
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-27 21:28:24 +0700

    Google API

--- 3. Asynchronous Programming/weather-app.js
+++ 3. Asynchronous Programming/weather-app.js
@@ -1,9 +1,10 @@
+const request = require('request');
 
-
-
-
-console.log('Starting app');
-setTimeout(() => {
-  console.log('Inside of callback');
-}, 2000);
-console.log('Finishing up');
+request({
+  url:'https://maps.googleapis.com/maps/api/geocode/json?address=bandung',
+  json: true
+}, (error, response, body) => {
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+});

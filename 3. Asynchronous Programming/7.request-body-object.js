f1c13b5b45dc7dccd15ed843723865126969d21c
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-28 22:09:32 +0700

    Get Body Object

--- 3. Asynchronous Programming/7.request-body-object.js
+++ 3. Asynchronous Programming/7.request-body-object.js
@@ -4,6 +4,6 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=bandung%20kota',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body));
+  console.log(`Address: ${body.results[0].formatted_address}`);
   //console.log(JSON.stringify(body, undefined, 2));
 });

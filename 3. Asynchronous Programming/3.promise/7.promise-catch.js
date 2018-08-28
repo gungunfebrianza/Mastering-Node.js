4ac67ceffaca3e1b957e85b6639af05f1833a0ce
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-29 00:23:40 +0700

    Promise Catch

--- 3. Asynchronous Programming/3.promise/7.promise-catch.js
+++ 3. Asynchronous Programming/3.promise/7.promise-catch.js
@@ -11,12 +11,10 @@ var asyncAdd = (a, b) => {
 };
 
 asyncAdd(5, 7).then((res) => {
-  console.log('Result:', res);
-  return asyncAdd(res, '33');
-}, (errorMessage) => {
-   console.log('error : ' + errorMessage);
- }).then((res) => {
-  console.log('Should be 45', res);
-}, (errorMessage) => {
-   console.log('error : ' + errorMessage);
- });
+ console.log('Result:', res);
+ return asyncAdd(res, '33');
+}).then((res) => {
+ console.log('Should be 45', res);
+}).catch((errorMessage) => {
+ console.log(errorMessage)
+});

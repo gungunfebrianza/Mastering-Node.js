e30cf3cfd0771c3504b77d7910f8e0c291b5d1fe
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-28 22:04:57 +0700

    Pretify Output

---
+++ 3. Asynchronous Programming/6.request-pretify.js
@@ -0,0 +1,8 @@
+const request = require('request');
+
+request({
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=bandung%20kota',
+  json: true
+}, (error, response, body) => {
+  console.log(JSON.stringify(body));
+});

0b5303642bc35646965e9f8ba3844df2df06c5f1
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-30 13:55:07 +0700

    Error Level Middleware

---
+++ 5. Express.js/2. Middleware/7.error-level-middleware-async.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var fs = require('fs')
+var app = express()
+
+app.get("/", function (req, res, next) {
+  fs.readFile("/file-does-not-exist`", function (err, data) {
+    if (err) {
+      next(err); // Pass errors to Express.
+    }
+    else {
+      res.send(data);
+    }
+  });
+});
+
+app.listen(9999, function() {
+  console.log('Listening on port 9999!');
+});

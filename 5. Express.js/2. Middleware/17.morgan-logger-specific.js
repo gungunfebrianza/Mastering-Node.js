4ec89c6704f1316c674d471e815ea5f1c0e163dc
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-30 20:37:41 +0700

    Morgan Logger Complete

---
+++ 5. Express.js/2. Middleware/17.morgan-logger-specific.js
@@ -0,0 +1,18 @@
+const express = require('express')
+var fs = require('fs')
+var morgan = require('morgan')
+var path = require('path')
+const app = express()
+
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
+
+// setup the logger
+app.use(morgan('combined', {
+  skip: function (req, res) { console.log(req.route.path)}
+}))
+
+app.get('*', (request, response, next) => {
+  response.send('Hello Morgan!')
+})
+
+app.listen(9999, () => console.log('Web Server running on port 9999'),)

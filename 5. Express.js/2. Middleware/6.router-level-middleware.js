493bacbda6e91e11dd6a83766a7692a4c69fd209
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-30 14:49:00 +0700

    Restructure Example

--- 5. Express.js/2. Middleware/6.router-level-middleware.js
+++ 5. Express.js/2. Middleware/6.router-level-middleware.js
@@ -3,7 +3,7 @@ const express = require('express');
 var app = express()
 var router = express.Router()
 
-var wiki = require('./4.router-module.js');
+var wiki = require('./5.router-module.js');
 app.use('/wiki', wiki);
 
 app.listen(9999, function() {

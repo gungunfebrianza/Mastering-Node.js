effd9ee208f5af08c5b17d0fe62817981e9a1994
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-08-30 13:25:04 +0700

    Application Level Middleware

--- 5. Express.js/2. Middleware/error_handling_sync.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var app = express()
-
-app.get("/", function (req, res) {
-  throw new Error("BROKEN"); // Express will catch this on its own.
-});
-
-app.listen(9999)

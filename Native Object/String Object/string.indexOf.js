000fc56882c18bf3924c36f4c3dc1a17f3e514b4
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-23 03:35:15 +0700

    Add String Object - indexof()

---
+++ Native Object/String Object/string.indexOf.js
@@ -0,0 +1,11 @@
+/* Definition and Usage
+
+The indexOf() method returns the position of the first occurrence of a specified value in a string.
+
+This method returns -1 if the value to search for never occurs.
+
+Note: The indexOf() method is case sensitive. */
+
+var str = "Hello world, welcome to the universe.";
+var n = str.indexOf("welcome");
+console.log(n);

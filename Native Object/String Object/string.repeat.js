5e32b149880744e2645b8fa19e1138ce797c3e71
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-23 03:39:05 +0700

    Add String Object - repeat()

---
+++ Native Object/String Object/string.repeat.js
@@ -0,0 +1,6 @@
+/* Definition and Usage
+
+The repeat() method returns a new string with a specified number of copies of the string it was called on. */
+
+var str = "Hello world!";
+console.log(str.repeat(2));

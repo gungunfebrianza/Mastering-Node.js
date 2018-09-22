41318b715d10c6ce7a794018b7d08ba10ee9dd3c
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-23 03:33:40 +0700

    Add String Object - concat()

---
+++ Native Object/String Object/string.concat.js
@@ -0,0 +1,10 @@
+/* Definition and Usage
+
+The concat() method is used to join two or more strings.
+
+This method does not change the existing strings, but returns a new string containing the text of the joined strings. */
+
+var str1 = "Hello ";
+var str2 = "world!";
+var res = str1.concat(str2);
+console.log(res);

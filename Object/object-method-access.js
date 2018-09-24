f5a3ad99b18ad9517e4b5e310f97b7751ab2a207
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-24 15:04:10 +0700

    Access Object Method

---
+++ Object/object-method-access.js
@@ -0,0 +1,10 @@
+var person = {
+  firstName: "John",
+  lastName: "Doe",
+  id: 5566,
+  fullName: function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
+console.log(person.fullName());

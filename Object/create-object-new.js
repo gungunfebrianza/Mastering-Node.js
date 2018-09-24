e76e45ca6b8c9bc85ead18e54e03a7f8f88cbc78
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-24 14:52:12 +0700

    New Keyword to Create Object

---
+++ Object/create-object-new.js
@@ -0,0 +1,12 @@
+/* 
+Using the JavaScript Keyword new
+
+The following example also creates a new JavaScript object with four properties: */
+
+var person = new Object();
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue";
+
+console.log(person);

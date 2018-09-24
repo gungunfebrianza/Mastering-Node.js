6ae05a81ed5f0e99de625dc83d5562890cee0a44
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-24 15:00:29 +0700

    Add Object Property Access

---
+++ Object/object-property-access.js
@@ -0,0 +1,9 @@
+var person = {
+  firstname: "John",
+  lastname: "Doe",
+  age: 50,
+  eyecolor: "blue"
+};
+
+console.log(person.firstname + " is " + person.age + " years old.");
+console.log(person["firstname"] + " is " + person["age"] + " years old.");

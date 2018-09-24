72d97aa1a39f3a6aad366174a0711826d377a301
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-24 14:50:34 +0700

    Creat Object Literal

---
+++ Object/create-object-literal.js
@@ -0,0 +1,10 @@
+/* This is the easiest way to create a JavaScript Object.
+
+Using an object literal, you both define and create an object in one statement.
+
+An object literal is a list of name:value pairs (like age:50) inside curly braces {}.
+
+The following example creates a new JavaScript object with four properties: */
+
+var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
+console.log(person);

4b10969a0840d9ba9f2986dc7ff8cc95d3492c54
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-25 14:39:00 +0700

    Add Array.unshift()

--- Native Object/Array Object/array.unshift.js
+++ Native Object/Array Object/array.unshift.js
@@ -1,7 +1,7 @@
 /* Definition and Usage
-
 The unshift() method adds new items to the beginning of an array, and returns the new length.
-
 Note: This method changes the length of an array.
-
 Tip: To add new items at the end of an array, use the push() method. */
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.unshift("Lemon", "Pineapple");

f2b8f6ff9d6d8fd1f5afbdd70be0c3bc6f96482e
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-09-30 18:11:43 +0700

    Add Array of Object Looping

---
+++ Native Object/Array Object/array-of-object-loop.js
@@ -0,0 +1,33 @@
+let products = [
+  {
+    code: 'P01',
+    name: 'Traditional Merlot',
+    description:
+      'A bottle of middle weight wine, lower in tannins (smoother), with a more red-fruited flavor profile.'
+  },
+  {
+    code: 'P02',
+    name: 'Classic Chianti',
+    description:
+      'A medium-bodied wine characterized by a marvelous freshness with a lingering, fruity finish'
+  },
+  {
+    code: 'P03',
+    name: 'Chardonnay',
+    description:
+      'A dry full-bodied white wine with spicy, bourbon-y notes in an elegant bottle'
+  },
+  {
+    code: 'P04',
+    name: 'Brunello di Montalcino',
+    description:
+      'A bottle of red wine with exceptionally bold fruit flavors, high tannin, and high acidity'
+  }
+];
+
+let productComponents = [];
+
+for (let product of products) {
+  //productComponents.push(<Product />);
+  console.log(product);
+}

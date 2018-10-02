c34e6611cfef836547dc7626ab55f7716a92fcee
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-02 20:25:12 +0700

    Add Array.map Method

---
+++ Native Object/Array Object/array.map.js
@@ -0,0 +1,25 @@
+/* Definition and Usage
+The filter() method creates an array filled with 
+all array elements that pass a test (provided as a function). 
+
+Note: filter() does not execute the function for array elements without values.
+Note: filter() does not change the original array.
+*/
+
+/* Syntax
+array.filter(function(currentValue, index, arr), thisValue) */
+
+/* Return Value: 	
+An Array containing all the array elements that pass the test. If no elements pass the test it returns an empty array. */
+
+/* Argument 	Description
+currentValue 	Required. The value of the current element
+index 	Optional. The array index of the current element
+arr 	Optional. The array object the current element belongs to */
+
+const numbers = [4, 9, 16, 25];
+const over18 = numbers.filter((value, index, array) => {
+  return value > 18;
+});
+
+console.log(over18);

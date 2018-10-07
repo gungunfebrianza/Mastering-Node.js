f4f8571e20de3629fd966b0dd734892fe341d123
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-08 00:09:36 +0700

    Add 1 Parameter Date

---
+++ Native Object/Date Object/date-1-parameter.js
@@ -0,0 +1,7 @@
+// One parameter will be interpreted as new Date(milliseconds).
+
+var d = new Date(2018);
+console.log(d);
+
+// Output :
+// Thu Jan 01 1970 07:00:02 GMT+0700 (Western Indonesia Time)

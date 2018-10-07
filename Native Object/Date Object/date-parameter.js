5361907dc88ed2e251219590c736070c93967c26
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-08 00:07:25 +0700

    Add Date Parameter Example

---
+++ Native Object/Date Object/date-parameter.js
@@ -0,0 +1,10 @@
+/* 
+new Date(year, month, ...) creates a new date object with a specified date and time.
+7 numbers specify year, month, day, hour, minute, second, and millisecond (in that order): 
+*/
+
+var d = new Date(2018, 11, 24, 10, 33, 30, 0);
+console.log(d);
+
+// Output :
+// Mon Dec 24 2018 10:33:30 GMT+0700 (Western Indonesia Time)

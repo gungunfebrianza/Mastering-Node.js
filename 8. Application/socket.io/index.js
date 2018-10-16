3f139a79f467ae3fea3298aaa3621acc85531a97
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-17 02:05:14 +0700

    Add Socket.io Example

--- 8. Application/socket.io/index.js
+++ 8. Application/socket.io/index.js
@@ -1,8 +1,27 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http, {
+  serveClient: true,
+  // below are engine.IO options
+  pingInterval: 5000,
+  pingTimeout: 5000,
+  cookie: false
+});
 
-app.get('/', function(req, res) {
+app.get('/test', function(req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socketCustomerService) {
+  console.log('a user connected');
+  socketCustomerService.on('chat message', function(msg) {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+
+  socketCustomerService.on('disconnect', function() {
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function() {

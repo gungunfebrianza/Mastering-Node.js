23a6b574ac19bdbd1ec2b6942698f55b792168de
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-16 20:40:42 +0700

    Add Array.dropRight

---
+++ 8. Application/lodashApp/array.dropRight.js
@@ -0,0 +1,15 @@
+/* Definition :
+Creates a slice of array with n elements dropped from the end. */
+var _ = require('lodash');
+
+_.dropRight([1, 2, 3]);
+// => [1, 2]
+
+_.dropRight([1, 2, 3], 2);
+// => [1]
+
+_.dropRight([1, 2, 3], 5);
+// => []
+
+_.dropRight([1, 2, 3], 0);
+// => [1, 2, 3]

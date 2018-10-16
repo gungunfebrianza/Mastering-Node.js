d60bd8fa17158db2c084f5728c572896e9363cfe
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-17 04:42:03 +0700

    Add Template Engine #02

--- 8. Application/secure-chat/server.js
+++ 8. Application/secure-chat/server.js
@@ -2,26 +2,12 @@
 const express = require('express');
 const app = express();
 app.set('port', process.env.PORT || 3000);
-
-let testMiddleware = (req, res, next) => {
-  // Create new key with value pair
-  req.hello = 'Middleware';
-  // move to the next control
-  next();
-};
-
-// Use The Middleware Before hit all route.
-app.use(testMiddleware);
-// for specific route
-//app.use('/dashboard',testMiddleware);
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res, next) => {
-  res.send('<h1>Hello World</h1>');
-  console.log(req.hello);
-});
-
-app.get('/dashboard', (req, res, next) => {
-  res.send('<h1>Dashboard ' + req.hello + '</h1>');
+  res.render('login', {
+    pageTitle: 'Login Page'
+  });
 });
 
 app.listen(3000, () => {

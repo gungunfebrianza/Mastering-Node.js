5fe74356fdc955c5c822eb105c4b5d89a2408fc5
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-16 20:39:12 +0700

    Add Array.drop

---
+++ 8. Application/lodashApp/array.drop.js
@@ -0,0 +1,15 @@
+/* Definition :
+Creates a slice of array with n elements dropped from the beginning */
+var _ = require('lodash');
+
+_.drop([1, 2, 3]);
+// => [2, 3]
+
+_.drop([1, 2, 3], 2);
+// => [3]
+
+_.drop([1, 2, 3], 5);
+// => []
+
+_.drop([1, 2, 3], 0);
+// => [1, 2, 3]

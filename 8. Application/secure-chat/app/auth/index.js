f33bfa09eab838339135a1bf7f1ec7d5e7c3b834
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-17 23:52:36 +0700

    Integrate MongoDB with Facebook OAuth

--- 8. Application/secure-chat/app/auth/index.js
+++ 8. Application/secure-chat/app/auth/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const passport = require('passport');
 const config = require('../config');
+const h = require('../helpers');
 const FacebookStrategy = require('passport-facebook').Stragegy;
 
 module.exports = () => {
@@ -8,6 +9,16 @@ module.exports = () => {
     //find a user in the local db using profile.id
     //if user found, return the user data using done
     //if not found, create one in the local db and return
+    h.findOne(profile.id).then(result => {
+      if (result) {
+        done(null, result);
+      } else {
+        // create new user and return
+        h.createNewUser(profile)
+          .then(createNewUser => done(null, createNewUser))
+          .catch(error => console.log('error while creating new user'));
+      }
+    });
   };
   passport.use(new FacebookStrategy(config.fb, authProcessor));
 };
--- 8. Application/secure-chat/app/helpers/index.js
+++ 8. Application/secure-chat/app/helpers/index.js
@@ -1,5 +1,6 @@
 'use strict';
 const router = require('express').Router();
+const db = require('../db');
 
 let _registerRoutes = (routes, method) => {
   for (let key in routes) {
@@ -46,6 +47,31 @@ let route = routes => {
   return router;
 };
 
+let findOne = profileID => {
+  return db.userModel.findOne({
+    profileId: profileID
+  });
+};
+
+let createNewUser = profile => {
+  return new Promise((resolve, reject) => {
+    let newChatUser = new db.userModel({
+      profileId: profile.id,
+      fullName: profile.displayName,
+      profilePic: profile.photos[0].value || ''
+    });
+    newChatUser.save(error => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(newChatUser);
+      }
+    });
+  });
+};
+
 module.exports = {
-  route: route
+  route,
+  findOne,
+  createNewUser
 };

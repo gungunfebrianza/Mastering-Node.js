2c8e1a042c994ac113f3373f2c366acab1daed77
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-19 02:55:05 +0700

    Return Promise Resolve

--- 4. Asynchronous/async-await/async-function.js
+++ 4. Asynchronous/async-await/async-function.js
@@ -2,5 +2,12 @@ async function f() {
   return 1;
 }
 
+async function g() {
+  return Promise.resolve(1);
+}
+
 f().then(result => console.log(result)); 
 // 1
+
+g().then(result => console.log(result)); 
+//same output promise : 1

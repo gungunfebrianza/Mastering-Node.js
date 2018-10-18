ed0f40d19c75ee757654573898bec780d53f10f0
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-19 02:56:28 +0700

    Unhandled Promise Rejection

--- 4. Asynchronous/async-await/async-function.js
+++ 4. Asynchronous/async-await/async-function.js
@@ -5,9 +5,15 @@ async function f() {
 async function g() {
   return Promise.resolve(1);
 }
+async function h() {
+  return Promise.reject(1);
+}
 
-f().then(result => console.log(result)); 
+f().then(result => console.log(result));
 // 1
 
-g().then(result => console.log(result)); 
+g().then(result => console.log(result));
 //same output promise : 1
+
+h().then(result => console.log(result));
+//unhandled promise rejection

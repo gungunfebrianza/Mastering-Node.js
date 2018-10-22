a506497559303129e1b9d4f73e0c5c428fda4c6f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-23 01:24:28 +0700

    Disconnect Socket

--- 8. Application/socket.io/index.js
+++ 8. Application/socket.io/index.js
@@ -9,23 +9,25 @@ app.get('/test', function(req, res) {
 io.on('connection', function(socketCustomerService) {
   console.log('a user connected');
 
-  socketCustomerService.join('room 237', msg => {
-    let rooms = Object.keys(socketCustomerService.rooms);
-    console.log(rooms); // [ <socket.id>, 'room 237' ]
-    console.log(socketCustomerService);
+  // socketCustomerService.join('room 237', msg => {
+  //   let rooms = Object.keys(socketCustomerService.rooms);
+  //   console.log(rooms); // [ <socket.id>, 'room 237' ]
+  //   //console.log(socketCustomerService);
 
-    socketCustomerService.on('private message', function(msg) {
-      console.log('privates message: ' + msg);
-      // sending to all connected clients
-      io.emit('chat message', msg);
-    });
-  });
-
-  // socketCustomerService.on('private message', function(msg) {
-  //   console.log('privates message: ' + msg);
-  //   // sending to all connected clients
   //   io.emit('chat message', msg);
+
+  //   socketCustomerService.on('chat message', function(msg) {
+  //     console.log('chat message: ' + msg);
+  //     // sending to all connected clients
+  //     io.emit('chat message', msg);
+  //   });
   // });
+
+  socketCustomerService.on('hello', function(msg) {
+    console.log('privates message: ' + msg);
+    // sending to all connected clients
+    io.emit('chat message', msg);
+  });
 
   socketCustomerService.on('disconnect', function() {
     console.log('user disconnected');

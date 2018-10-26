b0bfd4716e670032f527c17acf2db1da023756b1
gungunfebrianza <pusatrisetteknologi@gmail.com>
2018-10-27 00:41:29 +0700

    Add Mongoose

--- 8. Application/socket.io/index.js
+++ 8. Application/socket.io/index.js
@@ -1,7 +1,27 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const dbURL =
+  'mongodb://root:roottoor2019@ds247410.mlab.com:47410/securechatdb';
+
+mongoose.connect(
+  dbURL,
+  err => {
+    if (err) console.log(err);
+    console.log('Connected to Mlab');
+  }
+);
+
+var Message = mongoose.model('Message', {
+  name: String,
+  message: String
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/test', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
@@ -10,7 +30,7 @@ var clients = 0;
 
 // 1. Detect Socket
 io.on('connection', function(clientsSocket) {
-  console.log('a user connected');
+  console.log('A User Connected! ' + clientsSocket.id);
 
   // 2. Initialize Default Username
   clientsSocket.username = 'Anonymous';
@@ -27,25 +47,49 @@ io.on('connection', function(clientsSocket) {
   // 5. Send Server Time
   setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
+  // clientsSocket.on('register', handleRegister);
+
+  // clientsSocket.on('join', handleJoin);
+
+  // clientsSocket.on('leave', handleLeave);
+
+  // clientsSocket.on('message', handleMessage);
+
+  // clientsSocket.on('chatrooms', handleGetChatrooms);
+
+  // clientsSocket.on('availableUsers', handleGetAvailableUsers);
+
+  // Change Username
   clientsSocket.on('change_username', data => {
     clientsSocket.username = data.username;
     console.log(data.username);
   });
 
+  // Chat Event
   clientsSocket.on('chat', function(data) {
     console.log('message: ' + data.message);
+    const objmsg = {
+      name: data.clientsSocket.username,
+      message: data.message
+    };
+    const msg = new Message(objmsg);
+    msg.save(err => {
+      if (err) sendStatus(500);
+    });
     io.sockets.emit('chat', {
       message: data.message,
       username: clientsSocket.username
     });
   });
 
+  // Event Typing
   clientsSocket.on('typing', data => {
     clientsSocket.broadcast.emit('typing', {
       username: clientsSocket.username
     });
   });
 
+  // Detect Disconnect
   clientsSocket.on('disconnect', function() {
     console.log('user disconnected');
     clients--;
@@ -53,6 +97,12 @@ io.on('connection', function(clientsSocket) {
     clientsSocket.broadcast.emit('broadcast', {
       description: clients + ' clients connected! broadcast!'
     });
+  });
+
+  // Detect Error
+  clientsSocket.on('error', function(err) {
+    console.log('received error from client:', clientsSocket.id);
+    console.log(err);
   });
 });
 

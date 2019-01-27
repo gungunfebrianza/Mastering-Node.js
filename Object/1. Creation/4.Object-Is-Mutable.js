b82c3d8481b11255343d8c0e1bd45dd3f2810b11
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-01-27 23:06:07 +0700

    Object Mutability

--- Object/1. Creation/4.Object-Is-Mutable.js
+++ Object/1. Creation/4.Object-Is-Mutable.js
@@ -5,8 +5,9 @@ If person is an object, the following statement will not create a copy of person
 The object x is not a copy of person. It is person. Both x and person are the same object.
 Any changes to x will also change person, because x and person are the same object.  */
 
-var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
+var person = { firstName: 'John', lastName: 'Doe', age: 50, eyeColor: 'blue' };
 
 var x = person;
 x.age = 10;
 console.log(x);
+console.log(person); //changed too!

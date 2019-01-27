159ccb5e1a04588f2469c83e8b955c47a66ada9f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-01-27 23:52:58 +0700

    Nested Loop

--- Object/4. Loop/1.Object-Property-Loop.js
+++ Object/4. Loop/1.Object-Property-Loop.js
@@ -1,8 +1,6 @@
-var txt = "";
-var person = { fname: "John", lname: "Doe", age: 25 };
+var person = { fname: 'John', lname: 'Doe', age: 25 };
 var x;
 for (x in person) {
-  //console.log((txt += person[x] + " "));
   console.log(x);
   console.log(person[x]);
 }
---
+++ Object/4. Loop/2.Object-Nested-Loop.js
@@ -0,0 +1,39 @@
+let routes = {
+  getLevel1: {
+    getLevel2A: 'Get Level 2 A',
+    getLevel2B: 'Get Level 2 B',
+    getLevel2C: 'Get Level 2 C'
+  },
+  postLevel1: {
+    postLevel2A: 'Post Level 2 A',
+    postLevel2B: 'Post Level 2 B',
+    postLevel2C: 'Post Level 2 C'
+  },
+  NALevel1: 'NA'
+};
+
+let levelOne = routes => {
+  console.log('===== Level One ======');
+  for (let getLevel1 in routes) {
+    console.log(getLevel1);
+    console.log(routes[getLevel1]);
+  }
+  console.log('===== Level One ======');
+};
+
+levelOne(routes);
+
+let levelTwo = routes => {
+  console.log('===== Level Two ======');
+  for (let getLevel1 in routes) {
+    // console.log(getLevel1);
+    // console.log(routes[getLevel1]);
+    for (let getLevel2 in routes[getLevel1]) {
+      console.log(getLevel2);
+      console.log(routes[getLevel1][getLevel2]);
+    }
+  }
+  console.log('===== Level Two ======');
+};
+
+levelTwo(routes);

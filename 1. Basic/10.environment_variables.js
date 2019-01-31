7f09f862dd0f4317e368cb172bc6e375050e6f33
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-02-01 03:02:58 +0700

    Add Environment Variables

---
+++ 1. Basic/10.environment_variables.js
@@ -0,0 +1,3 @@
+console.log(process.env.NODE_ENV, process.env.API_KEY, process.env.DB_PASSWORD);
+
+//Then, before the application is started, set these variables: $ NODE_ENV=test API_KEY=XYZ DB_PASSWORD=ABC node 10.environment_variables.js

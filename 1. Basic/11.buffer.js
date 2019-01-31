5d7d4e34b0df311276e79fa125241eb9cac243bf
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-02-01 03:12:52 +0700

    Buffer Change

--- 1. Basic/11.buffer.js
+++ 1. Basic/11.buffer.js
@@ -1,13 +1,18 @@
-const bufFromArray = Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]) 
-console.log(bufFromArray.toString()) 
+const bufFromArray = Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]);
+console.log(bufFromArray.toString());
 
-// "buffer" 
-const arrayBuffer = new Uint16Array(2) 
-arrayBuffer[0] = 5 
-arrayBuffer[1] = 7000 
+// "buffer"
+const arrayBuffer = new Uint16Array(2);
+arrayBuffer[0] = 5;
+arrayBuffer[1] = 7000;
 
-// Shares memory with `arrayBuffer` 
-const bufFromArrayBuffer = Buffer.from(arrayBuffer.buffer) 
+// Shares memory with `arrayBuffer`
+const bufFromArrayBuffer = Buffer.from(arrayBuffer.buffer);
 
-// Prints: <Buffer 05 00 58 1b> 
-console.log(bufFromArrayBuffer)
+// Prints: <Buffer 05 00 58 1b>
+console.log(bufFromArrayBuffer);
+
+// Changing the original Uint16Array changes the Buffer also
+arrayBuffer[1] = 7001;
+// Prints: <Buffer 05 00 59 1b>
+console.log(bufFromArrayBuffer);

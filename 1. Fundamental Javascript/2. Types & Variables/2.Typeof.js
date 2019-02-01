17ba20f8350bc05b468434fe88caa262ea9d02bb
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-02-01 18:05:21 +0700

    Typeof Example

---
+++ 1. Fundamental Javascript/2. Types & Variables/2.Typeof.js
@@ -0,0 +1,18 @@
+var years = 2018;
+var pi = 3.14;
+//JavaScript represents numbers using the 64-bit floating-point format defined by the IEEE 754 standard.
+
+var maudy = 'Maudy Ayunda';
+var logic = true;
+let gun = {
+  nama: 'Gun Gun Febrianza'
+};
+let nullvar = null;
+let _undefined;
+
+console.log(typeof years);
+console.log(typeof pi);
+console.log(typeof gun);
+console.log(typeof gun.nama);
+console.log(typeof nullvar);
+console.log(typeof _undefined);

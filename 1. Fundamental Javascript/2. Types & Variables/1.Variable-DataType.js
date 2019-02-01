43267edbd2a6ecdd8e1e6a3903f4959761a8e2dd
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-02-01 18:04:08 +0700

    Variables & Data Type

---
+++ 1. Fundamental Javascript/2. Types & Variables/1.Variable-DataType.js
@@ -0,0 +1,43 @@
+var years = 2018;
+var pi = 3.14;
+/*JavaScript represents numbers using the 64-bit floating-point format defined by the IEEE 754 standard.
+      Besides regular numbers, there are so-called “special numeric values” which also belong to that type:
+      1. Infinity = represents the mathematical Infinity ∞. It is a special value that’s greater than any number.
+      2. -Infinity and
+      3. NaN = represents a computational error. It is a result of an incorrect or an undefined mathematical operation,
+      */
+
+var maudy = 'Maudy Ayunda';
+/*In JavaScript, there are 3 types of quotes.
+      1. Double quotes: "Hello".
+      2. Single quotes: 'Hello'.
+      3. Backticks: `Hello`.
+      */
+
+var logic = true;
+// The boolean type has only two values: true and false.
+
+let gun = {
+  nama: 'Gun Gun Febrianza'
+};
+/*All other types are called “primitive”, because their values can contain only a single thing (be it a string or a number or whatever).
+      In contrast, objects are used to store collections of data and more complex entities*/
+
+let nullvar = null;
+//The special null value does not belong to any type of those described above.
+
+let _undefined;
+//The meaning of undefined is “value is not assigned”.
+
+console.log(years);
+console.log(pi);
+console.log(gun);
+console.log(gun.nama);
+console.log(nullvar);
+console.log(_undefined);
+
+const COLOR_RED = '#F00';
+const COLOR_GREEN = '#0F0';
+const COLOR_BLUE = '#00F';
+const COLOR_ORANGE = '#FF7F00';
+//There is a widespread practice to use constants as aliases for difficult-to-remember values that are known prior to execution.

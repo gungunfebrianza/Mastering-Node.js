6feba1631aecb38136332d9746b1b5ff924eb24f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-10 04:36:41 +0700

    Number Precision

---
+++ 1. Fundamental Javascript/3. Number/3. Number Precision.js
@@ -0,0 +1,8 @@
+/* Integers (numbers without a period or exponent notation) are accurate up to 15 digits.
+Example */
+
+var x = 999999999999999; // x will be 999999999999999
+var y = 9999999999999999; // y will be 10000000000000000
+
+console.log(x);
+console.log(y);

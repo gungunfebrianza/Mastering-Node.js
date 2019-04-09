3da05444aa74b6b90028444a6e6157625f3489e5
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-10 04:39:04 +0700

    Number Precision Testing

--- 1. Fundamental Javascript/3. Number/3. Number Precision.js
+++ 1. Fundamental Javascript/3. Number/3. Number Precision.js
@@ -6,3 +6,12 @@ var y = 9999999999999999; // y will be 10000000000000000
 
 console.log(x);
 console.log(y);
+
+/* The maximum number of decimals is 17, but floating point arithmetic is not always 100% accurate:
+Example */
+var x = 0.2 + 0.1; // x will be 0.30000000000000004
+console.log(x);
+
+/* To solve the problem above, it helps to multiply and divide:
+Example */
+var x = (0.2 * 10 + 0.1 * 10) / 10; // x will be 0.3

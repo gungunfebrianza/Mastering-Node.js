59e6a9367e0ba7de62e082857709ec84b8f3e6e6
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-10 04:08:21 +0700

    a

--- 1. Fundamental Javascript/1. Start/2.Comment.js
+++ 1. Fundamental Javascript/1. Start/2.Comment.js
@@ -1,7 +1,11 @@
 // Single Line Comment
+// Short cut in atom is CTRL + /
 
 /*
       Multi
       Line
       Comment
       */
+
+/* To make multi comment shortcut 
+in atom is SHIFT + ALT + A */

03904ed87511e4f213890b27c922cc2a891dc6ef
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-10 04:29:21 +0700

    Javascript Number

---
+++ 1. Fundamental Javascript/3. Number/1. Javascript Number.js
@@ -0,0 +1,4 @@
+/* JavaScript has only one type of number. Numbers can be written with or without decimals.
+Example */
+var x = 3.14; // A number with decimals
+var y = 3; // A number without decimals

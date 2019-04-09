1df21d2c4f164c5532ab13db8f4fa58b7f6f6450
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-10 04:23:45 +0700

    Add Number Precision

---
+++ 1. Fundamental Javascript/3. Number/Number Precision.js
@@ -0,0 +1,10 @@
+var num = 13.3714;
+var a = num.toPrecision();
+var b = num.toPrecision(2);
+var c = num.toPrecision(3);
+var d = num.toPrecision(10);
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);

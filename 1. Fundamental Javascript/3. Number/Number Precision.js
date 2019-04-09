a9187a48b382944735d10edbb33215c19e15f10f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-10 04:26:31 +0700

    Add Description

--- 1. Fundamental Javascript/3. Number/Number Precision.js
+++ 1. Fundamental Javascript/3. Number/Number Precision.js
@@ -1,3 +1,7 @@
+/* Definition and Usage
+The toPrecision() method formats a number to a specified length.
+A decimal point and nulls are added (if needed), to create the specified length. */
+
 var num = 13.3714;
 var a = num.toPrecision();
 var b = num.toPrecision(2);
@@ -8,3 +12,9 @@ console.log(a);
 console.log(b);
 console.log(c);
 console.log(d);
+
+/* Result 
+13.3714;
+13;
+13.4;
+13.3714; */

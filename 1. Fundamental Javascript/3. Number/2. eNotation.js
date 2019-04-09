d7fbc46184263c6b74e2cf28642d7a6a7c12dd4c
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-10 04:30:58 +0700

    Add e Notation

---
+++ 1. Fundamental Javascript/3. Number/2. eNotation.js
@@ -0,0 +1,7 @@
+/* Extra large or extra small numbers can be written with scientific (exponent) notation:
+Example */
+var x = 123e5; // 12300000
+var y = 123e-5; // 0.00123
+
+console.log(x);
+console.log(y);

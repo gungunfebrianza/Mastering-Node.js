fd059e23c819cf5306371ba5c6ebca661dfb74a9
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-10 22:30:43 +0700

    Precision Handling

--- 1. Fundamental Javascript/3. Number/24. Precision Handling.js
+++ 1. Fundamental Javascript/3. Number/24. Precision Handling.js
@@ -31,3 +31,20 @@ console.log(precise(0.004));
 
 console.log(precise('1.23e+5'));
 // expected output: "1.230e+5"
+
+var numObj = 5.123456;
+
+console.log(numObj.toPrecision()); // logs '5.123456'
+console.log(numObj.toPrecision(5)); // logs '5.1235'
+console.log(numObj.toPrecision(2)); // logs '5.1'
+console.log(numObj.toPrecision(1)); // logs '5'
+
+numObj = 0.000123;
+
+console.log(numObj.toPrecision()); // logs '0.000123'
+console.log(numObj.toPrecision(5)); // logs '0.00012300'
+console.log(numObj.toPrecision(2)); // logs '0.00012'
+console.log(numObj.toPrecision(1)); // logs '0.0001'
+
+// note that exponential notation might be returned in some circumstances
+console.log((1234.5).toPrecision(2)); // logs '1.2e+3'

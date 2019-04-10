ffd750003230600151963e8a70be6bde68313c78
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-10 11:06:36 +0700

    MIN & MAX Value

--- 1. Fundamental Javascript/3. Number/11. MAX_VALUE.js
+++ 1. Fundamental Javascript/3. Number/11. MAX_VALUE.js
@@ -10,3 +10,6 @@ console.log(multiply(1.7976931348623157e308, 1));
 
 console.log(multiply(1.7976931348623157e308, 2));
 // expected output: "Process as Infinity"
+
+/* The Number.MAX_VALUE property represents the maximum numeric value representable in JavaScript.
+The MAX_VALUE property has a value of approximately 1.79E+308, or 21024. Values larger than MAX_VALUE are represented as Infinity. */

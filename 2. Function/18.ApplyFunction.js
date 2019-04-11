63299b272dd2d1f3a40cd850f2e51546f7a4ed5c
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-11 09:40:50 +0700

    ApplyFunction

--- 2. Function/18.ApplyFunction.js
+++ 2. Function/18.ApplyFunction.js
@@ -1,29 +1,11 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-
-<head>
-  <meta charset="utf-8">
-  <title>Apply Function</title>
-</head>
-
-<body>
-  <h2>JavaScript Functions</h2>
-  <p>In this example the fulllName method of person is <b>applied</b> on person1:</p>
-
-  <p id="demo"></p>
-</body>
-<script>
-  var person = {
-    fullName: function() {
-      return this.firstName + " " + this.lastName;
-    }
+var person = {
+  fullName: function() {
+    return this.firstName + ' ' + this.lastName;
   }
-  var person1 = {
-    firstName: "John",
-    lastName: "Doe",
-  }
-  var x = person.fullName.apply(person1);
-  document.getElementById("demo").innerHTML = x;
-</script>
-
-</html>
+};
+var person1 = {
+  firstName: 'John',
+  lastName: 'Doe'
+};
+var x = person.fullName.apply(person1);
+console.log(x);

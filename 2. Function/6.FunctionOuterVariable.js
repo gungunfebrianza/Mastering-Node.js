026e1514da3cf05465423952396e867013e0461f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-11 09:28:18 +0700

    Function Outer Variable

--- 2. Function/6.FunctionOuterVariable.js
+++ 2. Function/6.FunctionOuterVariable.js
@@ -1,25 +1,8 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
+let userName = 'John';
 
-<head>
-  <meta charset="utf-8">
-  <title>Function Outer Variable</title>
-</head>
+function showMessage() {
+  let message = 'Hello, ' + userName;
+  console.log(message);
+}
 
-<body>
-  <h2>JavaScript Functions</h2>
-  <p>This example calls a function which performs a calculation, and returns the result:</p>
-  <p id="demo"></p>
-</body>
-<script>
-  let userName = 'John';
-
-  function showMessage() {
-    let message = 'Hello, ' + userName;
-    alert(message);
-  }
-
-  showMessage(); // Hello, my name is John
-</script>
-
-</html>
+showMessage(); // Hello, my name is John

d58fa54f9f75c102c636bb209a8408082fa0f5d8
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-11 09:23:30 +0700

    Function Parameter

---
+++ 2. Function/2.FunctionParameter.js
@@ -0,0 +1,7 @@
+function showMessage(from, text) {
+  // arguments: from, text
+  alert(from + ': ' + text);
+}
+
+showMessage('Ann', 'Hello!'); // Ann: Hello! (*)
+showMessage('Ann', "What's up?"); // Ann: What's up? (**)

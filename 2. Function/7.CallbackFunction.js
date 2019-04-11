33ec4eb2ec3b294818fda7295eade4965f9af3cf
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-11 09:29:35 +0700

    Function Example

--- 2. Function/7.CallbackFunction.js
+++ 2. Function/7.CallbackFunction.js
@@ -1,32 +1,15 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
 
-<head>
-  <meta charset="utf-8">
-  <title>Callback Function</title>
-</head>
+function showOk() {
+  console.log('You agreed.');
+}
 
-<body>
+function showCancel() {
+  console.log('You canceled the execution.');
+}
 
-</body>
-<script type="text/javascript">
-  function ask(question, yes, no) {
-    if (confirm(question))
-      yes()
-    else
-      no();
-  }
-
-  function showOk() {
-    alert("You agreed.");
-  }
-
-  function showCancel() {
-    alert("You canceled the execution.");
-  }
-
-  // usage: functions showOk, showCancel are passed as arguments to ask
-  ask("Do you agree?", showOk, showCancel);
-</script>
-
-</html>
+// usage: functions showOk, showCancel are passed as arguments to ask
+ask('Do you agree?', showOk, showCancel);
--- 2. Function/8.ArrowFunction.js
+++ 2. Function/8.ArrowFunction.js
@@ -1,25 +1,10 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
+let sum = (a, b) => a + b;
 
-<head>
-  <meta charset="utf-8">
-  <title>Arrow Function</title>
-</head>
-
-<body>
-
-</body>
-<script type="text/javascript">
-  let sum = (a, b) => a + b;
-
-  /* The arrow function is a shorter form of:
+/* The arrow function is a shorter form of:
 
   let sum = function(a, b) {
   return a + b;
   };
   */
 
-  alert(sum(1, 2)); // 3
-</script>
-
-</html>
+console.log(sum(1, 2)); // 3

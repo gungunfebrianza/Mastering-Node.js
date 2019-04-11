b2de186b41008fe00691f63b95e14afb5b9fc2c0
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-11 09:03:32 +0700

    Else IF Control

---
+++ 1. Fundamental Javascript/7. Conditional Operation/3.else-if.js
@@ -0,0 +1,10 @@
+var book = 'maths';
+if (book == 'history') {
+  console.log('History Book');
+} else if (book == 'maths') {
+  console.log('Maths Book');
+} else if (book == 'economics') {
+  console.log('Economics Book');
+} else {
+  console.log('Unknown Book');
+}

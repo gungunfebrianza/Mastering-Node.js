95cf49d6988f2a12d7b4bac3c763b0eb77a5028a
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-23 02:35:38 +0700

    Access Multiword Properties

--- 3. Object/2. Properties/1.Object-Property-Access.js
+++ 3. Object/2. Properties/1.Object-Property-Access.js
@@ -1,9 +1,11 @@
 var person = {
-  firstname: "John",
-  lastname: "Doe",
-  age: 50,
-  eyecolor: "blue"
+  firstname: 'Gun Gun',
+  lastname: 'Febrianza',
+  age: 28,
+  eyecolor: 'Red Brown',
+  'likes birds': true // multiword property name must be quoted
 };
 
-console.log(person.firstname + " is " + person.age + " years old.");
-console.log(person["firstname"] + " is " + person["age"] + " years old.");
+console.log(person.firstname + ' is ' + person.age + ' years old.');
+console.log(person['firstname'] + ' is ' + person['age'] + ' years old.');
+console.log(person['likes birds']);

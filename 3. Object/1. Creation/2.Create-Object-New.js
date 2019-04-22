0b5a2326d0f0c4f6aa5821e43dd30df56583e54f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-23 02:26:42 +0700

    Add New Example

--- 3. Object/1. Creation/2.Create-Object-New.js
+++ 3. Object/1. Creation/2.Create-Object-New.js
@@ -3,9 +3,17 @@ Using the JavaScript Keyword new
 The following example also creates a new JavaScript object with four properties: */
 
 var person = new Object();
-person.firstName = "John";
-person.lastName = "Doe";
+person.firstName = 'John';
+person.lastName = 'Doe';
 person.age = 50;
-person.eyeColor = "blue";
+person.eyeColor = 'blue';
 
 console.log(person);
+
+//Example :
+//[1]
+
+var car = new Object();
+car.speed = 900;
+car.type = 'mazda';
+car.color = 'red';

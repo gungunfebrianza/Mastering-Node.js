0f6b7e25b3fda391ec6e0a41b9c2f14a1463b367
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-23 02:25:25 +0700

    Add New Example

--- 3. Object/1. Creation/1.Create-Object-Literal.js
+++ 3. Object/1. Creation/1.Create-Object-Literal.js
@@ -8,3 +8,12 @@ var person = {
 
 console.log('firstName : ' + person.firstName);
 console.log('Age : ' + person.age);
+
+// Example :
+// [1]
+var car = {
+  type: 'mazda',
+  color: 'red',
+  speed: 900
+};
+console.log(car.type);

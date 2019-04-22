594b330980b51ec11185fa47dfbdcb010c4569dc
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-04-23 02:39:40 +0700

    Delete

--- 3. Object/2. Properties/3.Object-Property-Delete.js
+++ 3. Object/2. Properties/3.Object-Property-Delete.js
@@ -1,3 +1,8 @@
-var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
+var person = {
+  firstName: 'Gun Gun',
+  lastName: 'Febrianza',
+  age: 28,
+  eyeColor: 'Red Brown'
+};
 delete person.age; // or delete person["age"];
 console.log(person);

b08ec8f683863c5651c7fca070f64361cdda51ad
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-05-11 11:54:07 +0700

    Generate PIN

---
+++ 15.Snippet/generatePin.js
@@ -0,0 +1,11 @@
+function makePin(length) {
+  var result = '';
+  var characters = '0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+console.log(makePin(6));

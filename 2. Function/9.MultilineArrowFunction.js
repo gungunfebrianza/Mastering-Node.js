7f0aa23c047cdaca8f6f1a53ce1e200506f15dfc
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-05-18 19:02:42 +0700

    Multiline Arrow

--- 2. Function/9.MultilineArrowFunction.js
+++ 2. Function/9.MultilineArrowFunction.js
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
 
-<head>
-  <meta charset="utf-8">
-  <title>Multiline Arrow Function</title>
-</head>
-
-<body>
-
-</body>
-<script type="text/javascript">
   let sum = (a, b) => { // the curly brace opens a multiline function
     let result = a + b;
     return result; // if we use curly braces, use return to get results
   };
 
   alert(sum(1, 2)); // 3
-</script>
-
-</html>

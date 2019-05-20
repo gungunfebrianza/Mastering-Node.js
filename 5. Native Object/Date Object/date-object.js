cd2b0bd322cd1a8c80abd50d565f8d6b1754e52f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-05-20 23:01:07 +0700

    Add Example

--- 5. Native Object/Date Object/date-object.js
+++ 5. Native Object/Date Object/date-object.js
@@ -1,5 +1,10 @@
 var d = new Date();
-console.log(d);
+//console.log(d);
 
 // Output :
 // 2018-10-07T16:47:04.580Z
+
+var strDate = '13-07-1995';
+var dateParts = strDate.split("-");
+var date = new Date(dateParts[2], (dateParts[1] - 1), dateParts[0]);
+console.log(date);

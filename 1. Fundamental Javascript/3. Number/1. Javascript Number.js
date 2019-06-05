5776b11591c87456c28e361c299075200ad5ad1c
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-06-05 22:41:13 +0700

    Add Example

--- 1. Fundamental Javascript/3. Number/1. Javascript Number.js
+++ 1. Fundamental Javascript/3. Number/1. Javascript Number.js
@@ -2,3 +2,7 @@
 Example */
 var x = 3.14; // A number with decimals
 var y = 3; // A number without decimals
+
+//Extra large or extra small numbers can be written with scientific(exponent) notation:
+var x = 123e5; // 12300000
+var y = 123e-5; // 0.00123 

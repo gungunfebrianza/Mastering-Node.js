17fc575759e7b54873ea2c14f397cea1daedcfe1
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-06-19 04:24:35 +0700

    New Update

--- 1. Fundamental Javascript/3. Number/2. eNotation.js
+++ 1. Fundamental Javascript/3. Number/2. eNotation.js
@@ -5,3 +5,15 @@ var y = 123e-5; // 0.00123
 
 console.log(x);
 console.log(y);
+
+let billion = 1e9; //1 billion (1 & 9 zero)
+//1e3 = 1*1000
+//1.23e6 = 1.23 * 1000000 
+
+//explicit zero
+let ms = 0.000001;
+//implicit zero
+let ms = 1e-6;
+
+//1e-3 = 1 / 1000
+//1.23e-6 = 1.23 / 1000000

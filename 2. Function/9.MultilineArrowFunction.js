2878e8a4c306db7058c0827893f995fcf870b9d6
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-06-28 19:37:47 +0700

    Fix Comment

--- 2. Function/9.MultilineArrowFunction.js
+++ 2. Function/9.MultilineArrowFunction.js
@@ -1,8 +1,8 @@
 
-  let sum = (a, b) => { // curly brace untuk membuat multiline function
-    let result = a + b;
-    return result; // gunakan return untuk memproduksi hasil
-  };
+let sum = (a, b) => { // curly brace untuk membuat multiline function
+  let result = a + b;
+  return result; // gunakan return untuk memproduksi hasil
+};
 
 console.log(sum(1, 2)); // 3
 

b08d8e6c398fc34daf448c36e525fcb2c88a54f4
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-06-28 17:05:31 +0700

    Update

--- 2. Function/7.CallbackFunction.js
+++ 2. Function/7.CallbackFunction.js
@@ -1,15 +1,10 @@
-function ask(question, yes, no) {
-  if (confirm(question)) yes();
-  else no();
+function greeting(name) {
+  console.log('Hello ' + name);
 }
 
-function showOk() {
-  console.log('You agreed.');
+function processUserInput(callback) {
+  var name = 'Gun Gun Febrianza';
+  callback(name);
 }
 
-function showCancel() {
-  console.log('You canceled the execution.');
-}
-
-// usage: functions showOk, showCancel are passed as arguments to ask
-ask('Do you agree?', showOk, showCancel);
+processUserInput(greeting);

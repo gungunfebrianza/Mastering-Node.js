87fbf50d977b86887ce894a73446d32d38ff3a3e
gungunfebrianza <pusatrisetteknologi@gmail.com>
2019-07-13 16:19:57 +0700

    Add Result

--- 2. Function/15.ArgumentObject.js
+++ 2. Function/15.ArgumentObject.js
@@ -3,7 +3,8 @@ function sumAll() {
   var sum = 0;
   /*JavaScript functions have a built-in
 object called the arguments object. The argument object contains an array of the
-arguments used when the function was called (invoked). */ for (
+arguments used when the function was called (invoked). */
+  for (
     i = 0;
     i < arguments.length;
     i++
@@ -13,3 +14,4 @@ arguments used when the function was called (invoked). */ for (
   return sum;
 }
 console.log(sumAll(1, 123, 500, 115, 44, 88));
+//871

9009abbb3377cfb8155d0bc013d6837a660c13d3
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-01-26 16:08:04 +0700

    Update

--- 3. Object/1. Creation/1.Create-Object-Literal.js
+++ 3. Object/1. Creation/1.Create-Object-Literal.js
@@ -20,3 +20,30 @@ console.log(car.type);
 
 car.owner = 'gun gun febrianza';
 console.log(car.owner);
+
+var wallet = {
+  walletID: 1,
+  balance: 9000,
+  owner: "Gun Gun Febrianza",
+  history: [],
+  checkBalance: function () {
+    return 'Your Balance is : ' + this.balance
+  },
+  deposite: function (newbalance) {
+    this.balance += newbalance
+    return this.balance
+  },
+  sendMoney: function (params) {
+    return "Sending Money Now!"
+  },
+  receiveMoney: function (newbalance) {
+    this.balance += newbalance
+    this.history.push({ from: 'cheysa', ammount: newbalance})
+  }
+}
+
+wallet.deposite(1000)
+wallet.receiveMoney(500)
+console.log(wallet.history);
+console.log(wallet.deposite(1000));
+

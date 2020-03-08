edf54cc67e6c4c44d64043f181cceae421bfc6ac
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-03-09 01:58:10 +0700

    AddResult

--- 3. Object/4. Loop/1.Object-Property-Loop.js
+++ 3. Object/4. Loop/1.Object-Property-Loop.js
@@ -1,6 +1,14 @@
-var person = { fname: 'Gun GUn', lname: 'Febrianza', age: 28 };
+var person = { fname: "Gun Gun", lname: "Febrianza", age: 28 };
 var x;
 for (x in person) {
   console.log(x);
   console.log(person[x]);
 }
+/* 
+fname
+Gun GUn
+lname
+Febrianza
+age
+28 
+*/

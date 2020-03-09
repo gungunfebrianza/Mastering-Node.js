e582ee97c0a6fec3c21955e63116c68825eaabe2
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-03-10 02:30:45 +0700

    Update

--- 6. Basic/8.execute.js
+++ 6. Basic/8.execute.js
@@ -1,8 +1,8 @@
-const execute = require('child_process').exec;
+  const execute = require('child_process').exec;
 
-execute('dir', (err,stdout)=>{
-  if (err) {
-    return err;
-  }
-  console.log(stdout);
-})
+  execute('dd Update', (err,stdout)=>{
+    if (err) {
+      return err;
+    }
+    console.log(stdout);
+  })

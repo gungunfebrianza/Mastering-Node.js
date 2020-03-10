98cdcd3b457cb5ca50966dbbebcc7b0538a667e8
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-03-11 01:54:09 +0700

    update

--- 7. Modules/1.module.js
+++ 7. Modules/1.module.js
@@ -1,7 +1,7 @@
-module.exports.title = 'I am title \n';
-module.exports.calculation = ()=>{
-  console.log('calculation');
-}
+module.exports.title = "I am title \n";
+module.exports.calculation = () => {
+  console.log("calculation");
+};
 
 console.log(module.exports.title);
 console.log(module.exports.calculation());
--- 8. Filesystem/1.file_reading.js
+++ 8. Filesystem/1.file_reading.js
@@ -1,15 +1,14 @@
-const fs = require('fs');
+const fs = require("fs");
 
-fs.readdir('./', 'UTF-8', (err, content) => {
-  if (err)
-    return err;
+fs.readdir("./", "UTF-8", (err, content) => {
+  if (err) return err;
   console.log(content);
-})
+});
 
-fs.readFile('global.html', (err, content)=>{
+fs.readFile("global.html", (err, content) => {
   console.log(content);
-})
+});
 
-fs.readFile('global.html', 'UTF-8', (err, content)=>{
+fs.readFile("global.html", "UTF-8", (err, content) => {
   console.log(content);
-})
+});

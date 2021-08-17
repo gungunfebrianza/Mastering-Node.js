61eb028b50cf7d0ed047a16107d0ec18ecacd718
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-08-18 00:39:46 +0700

    add examples

--- 6. Basic/4.process.js
+++ 6. Basic/4.process.js
@@ -1,3 +1,19 @@
+//console.log(process);
+console.log(process.versions.node);
+console.log(process.versions.v8);
+console.log(process.versions.uv);
+console.log(process.arch);
+console.log(process.platform);
+//console.log(process.env);
+//console.log(process.env.Path);
+console.log(process.env.PROCESSOR_ARCHITECTURE); // 'AMD64'
+console.log(process.env.PROCESSOR_IDENTIFIER); // 'Intel64 Family 6 Model 158 Stepping 9, GenuineIntel'
+console.log(process.env.OS); // 'Windows_NT'
+console.log(process.env.USERNAME); // Gun Gun Febrianza
+console.log(process.env.USERPROFILE); // 'C:\\Users\\Gun Gun Febrianza'
+console.log(process.env.TEMP); // 'C:\\Users\\GUNGUN~1\\AppData\\Local\\Temp'
+console.log(process.pid); // Process ID
+console.log(process.title); // Source Execution
 console.log(process.argv);
 
 /*The process object is a global that provides information about, and control
@@ -19,3 +35,7 @@ node process test e --user
 3: e
 4: --user
 */
+
+console.log(process.memoryUsage.rss()); //memory usage
+// SVGAnimatedString
+// asdsad

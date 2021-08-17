372a868aae873a62109fc1a7ae01dd86b95bb2a9
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-08-18 01:26:14 +0700

    update example

--- 7. Modules/6.json.js
+++ 7. Modules/6.json.js
@@ -1,11 +1,12 @@
 var obj = {
- name: 'Andrew'
+ name: 'Maudy Ayunda'
 };
 var stringObj = JSON.stringify(obj);
 console.log(typeof stringObj);
 console.log(stringObj);
 
-var personString = '{"name": "Andrew","age": 25}';
+var personString = '{"name": "Maudy Ayunda","age": 25}';
 var person = JSON.parse(personString);
 console.log(typeof person);
 console.log(person);
+console.log(person.name);

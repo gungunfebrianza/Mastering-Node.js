9d2d3e3043c81979888ca8a2c9e35641a216d9f2
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-10-24 22:16:44 +0700

    update example

--- 14. Modern Javascript/numeric-separator.js
+++ 14. Modern Javascript/numeric-separator.js
@@ -1,3 +1,10 @@
 const price = 1_000_000_000;
 console.log(price * 2);
 //output : 2000000000
+
+let amount = 120_201_123.05; // 120,201,123.05
+let expense = 123_4500; // 1,234,500
+let fee = 12345_00; // 1,234,500
+console.log(amount);
+console.log(expense);
+console.log(fee);

8254b410e85304bea22b65686a1003c74088f2d9
gungunfebrianza <pusatrisetteknologi@gmail.com>
2021-10-24 14:28:30 +0700

    update replaceall

--- 14. Modern Javascript/es2021-replaceall.js
+++ 14. Modern Javascript/es2021-replaceall.js
@@ -4,3 +4,19 @@ let newStr = source.replaceAll("Maudy", "Maudya Ayunda Faza");
 console.log(newStr);
 
 //output : Maudya Ayunda Faza Cantik, Maudya Ayunda Faza Manis, Maudya Ayunda Faza Pintar
+
+let str = "Maudy Cantik, MAudy Manis, maudy Pintar";
+
+let pattern = /maudy/gi;
+
+newStr = str.replaceAll(pattern, function (match, offset, str) {
+  if (match === "MAudy") return "MAUDYA AYUNDA FAZA";
+  if (match === "Maudy") return "Maudya Ayunda Faza";
+  if (match === "maudy") return "maudya ayunda faza";
+  return "";
+});
+
+console.log(newStr);
+
+//output : Maudya Ayunda Faza Cantik, MAUDYA AYUNDA FAZA Manis, maudya ayunda faza Pintar
+a
